bbade000e88ffe310a6d867c92bd35d44ff66c94
Dawson Whitehead <dwahme1@gmail.com>
2021-04-15 17:55:37 -0500

    Fixed math

--- odds.py
+++ odds.py
@@ -3,14 +3,23 @@
 def american_to_prob(odds):
 
     if odds < 0:
-        return odds / (odds + 100)
+        return (-odds) / (-odds + 100)
 
     return 100 / (odds + 100)
+
+def get_true_prob(american_for, american_against):
+    real_for = american_to_prob(american_for)
+    real_against = 1 - american_to_prob(american_against)
+
+    print(real_for, real_against)
+    print((real_for + real_against) / 2)
+
+    return (real_for + real_against) / 2
 
 def american_to_mult(odds):
 
     if odds < 0:
-        return 100 / odds
+        return 100 / -odds
     
     return odds / 100
 
@@ -19,7 +28,12 @@ def kelly(mult, prob):
     return (prob * (mult + 1) - 1) / mult
 
 def run_odds(american_for, american_against, boost):
-    pass
+    prob = get_true_prob(american_for, american_against)
+    mult = american_to_mult(american_for + boost)
+    mult_orig = american_to_mult(american_for)
+    print(kelly(mult_orig, prob))
+
+    return kelly(mult, prob)
 
 if __name__ == "__main__":
-    pass
+    print(run_odds(2000, -715, 0))
